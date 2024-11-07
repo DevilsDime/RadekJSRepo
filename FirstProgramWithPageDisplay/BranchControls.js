b48a2f5d47fbefdee49bbada682e956266cd51fb
DevilsDime <r.zgryska@gmail.com>
2024-11-07 15:43:37 +0100

    Modification regarding Branching and Looping. Adding If, While and For

--- FirstProgramWithPageDisplay/BranchControls.js
+++ FirstProgramWithPageDisplay/BranchControls.js
@@ -1,1 +1,28 @@
 console.log("Upcoming changes");
+var box1 = document.getElementById("result1");
+var box2 = document.getElementById("result2");
+var i = 0;
+while (i<20)
+    {
+        if(i%2==0)
+        {
+            box2.innerHTML += "<br>" + i;           
+        }
+        else
+        {
+            box1.innerHTML += "<br>" + i;
+        }        
+        i++;
+    }
+
+var boxChess = document.getElementById("resultFOR")
+
+var x,y;
+for(y=0;y<=10;y++)
+    {
+        for(x=0;x<=10;x++)
+            {
+                boxChess.innerHTML += " " + x*y;
+            }
+        boxChess.innerHTML += "<br>";
+    }
